67b366fd98cc0dd7a45d3eb2bea62f84dfb94e10
phixyn <phixyn@gmail.com>
2018-07-30 23:07:16 +0100

    feat(CSV_Transpose): add command-line arguments to CSV_Transpose script

--- CSV_Transpose.py
+++ CSV_Transpose.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
+"""Transposes the contents of a CSV file and saves the output to a new CSV file.
 
-# TODO: Add CLI arguments for input and output filenames (and a help/usage argument too)
+Example usage:
+CSV_Transpose.py "input.csv" "output.json"
+"""
 
+
+__author__ = "Phixyn"
+__version__ = "v1.1.0"
+
+
+import argparse
 import csv
-a = zip(*csv.reader(open("CSVToTranspose.csv", "r")))
-csv.writer(open("output.csv", "w", newline="")).writerows(a)
+
+
+# Set up command line argument parser
+parser = argparse.ArgumentParser()
+parser.add_argument("input_file", action="store", default=None, help="Path/file name of the CSV file to be transposed.")
+parser.add_argument("output_file", action="store", default=None, help="Path/file name for the transposed CSV file.")
+parser.add_argument("--version", "-V", action="version", version="CSV Transposer {0}".format(__version__), help="Displays version information and exits.")
+args = vars(parser.parse_args())
+
+# Transpose CSV and store output in new file
+a = zip(*csv.reader(open(args["input_file"], "r")))
+csv.writer(open(args["output_file"], "w", newline="")).writerows(a)
