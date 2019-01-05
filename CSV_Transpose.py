cb76956aec86c9fd69dc450cf9f8be89d11a693e
phixyn <phixyn@gmail.com>
2019-01-05 23:06:52 +0000

    refactor(CSV_Transpose): use if __name__ == __main__

--- CSV_Transpose.py
+++ CSV_Transpose.py
@@ -1,26 +1,25 @@
 #!/usr/bin/env python3
-"""Transposes the contents of a CSV file and saves the output to a new CSV file.
+"""Transposes the contents of a CSV file and saves the output to a new file.
 
 Example usage:
-CSV_Transpose.py "input.csv" "output.json"
+CSV_Transpose.py "input.csv" "output.csv"
 """
 
-
 __author__ = "Phixyn"
-__version__ = "v1.1.0"
-
+__version__ = "v1.1.1"
 
 import argparse
 import csv
 
 
-# Set up command line argument parser
-parser = argparse.ArgumentParser()
-parser.add_argument("input_file", action="store", default=None, help="Path/file name of the CSV file to be transposed.")
-parser.add_argument("output_file", action="store", default=None, help="Path/file name for the transposed CSV file.")
-parser.add_argument("--version", "-V", action="version", version="CSV Transposer {0}".format(__version__), help="Displays version information and exits.")
-args = vars(parser.parse_args())
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file", action="store", default=None, help="Path/file name of the CSV file to be transposed.")
+    parser.add_argument("output_file", action="store", default=None, help="Path/file name for the transposed CSV file.")
+    parser.add_argument("--version", "-V", action="version", version="CSV Transposer {0}".format(__version__), help="Displays version information and exits.")
+    args = vars(parser.parse_args())
 
-# Transpose CSV and store output in new file
-a = zip(*csv.reader(open(args["input_file"], "r")))
-csv.writer(open(args["output_file"], "w", newline="")).writerows(a)
+    # Read from file and transpose CSV
+    transposedCSV = zip(*csv.reader(open(args["input_file"], "r")))
+    # Store output in new file
+    csv.writer(open(args["output_file"], "w", newline="")).writerows(transposedCSV)
